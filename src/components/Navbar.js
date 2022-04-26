d5253517de35d27f992be34df370e8c0082c085e
shoyalax0703 <shoya.lax@gmail.com>
2022-04-26 14:13:59 +0900

    Nabvar completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import mylogo from "../mylogo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={mylogo} alt="logo..." />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                home <span className="sr-only"></span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                about me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                my project
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

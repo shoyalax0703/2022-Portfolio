7ef9314e6905e797b60f88c095c5ae253a046692
shoyalax0703 <shoya.lax@gmail.com>
2022-04-27 04:19:48 +0900

    About Me completed

---
+++ src/components/AboutMe.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Me from "../me.jpg";
+
+const AboutMe = () => {
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-6 col-xm-12">
+<div className="photo-wrap mb-5">
+
+          <img className="profile-img" src={Me} alt="Me..." />
+</div>
+        </div>
+        <div className="col-lg-6 col-xm-12">
+          <h1 className="about-header">
+            about me
+          </h1>
+          <p>
+          Hello! I am Shoya who has a strong passion for talent development and tech. In addition to facilitation and project management, he can use his communication and multi-tasking skills cultivated by various experiences. In a diverse and fast-paced environment, I am capable to execute projects while enjoying open communication. Those performances will be demonstrated in remote and office environments
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
